9eb2f4dedebb6abe5f11444ac1d6f8cf8663be28
nemethildiko <ildiko.nemeth001@gmail.com>
2026-01-11 15:23:48 +0100

    Login ablak beállítása alkalmazás indulásakor

---
+++ Telefonszamok_Alap/LoginWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace Telefonszamok_Alap
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbUser.Text == "admin" && pbPass.Password == "admin")
+            {
+                MainWindow mw = new MainWindow();
+                mw.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Hibás felhasználónév vagy jelszó!",
+                    "Bejelentkezési hiba",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                );
+            }
+        }
+    }
+}
