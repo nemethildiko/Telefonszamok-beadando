e52c31a0ca76f071db7359473e69d159e9ceeeda
nemethildiko <ildiko.nemeth001@gmail.com>
2026-01-11 19:57:57 +0100

    DataGrid oszlopok letisztazasa, navigation propertyk elrejtese

--- Telefonszamok_Alap/MainWindow.xaml.cs
+++ Telefonszamok_Alap/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using System.Windows;
+using Microsoft.EntityFrameworkCore;
 using cnTelefonkonyv;
+using Models;
 
 namespace Telefonszamok_Alap
 {
@@ -11,7 +13,18 @@ namespace Telefonszamok_Alap
         public MainWindow()
         {
             InitializeComponent();
-            dgSzemelyek.ItemsSource = cn.enSzemelyek.ToList();
+
+            dgSzemelyek.ItemsSource = cn.enSzemelyek
+                .Include(x => x.enTelefonszamok)
+                .ToList();
+        }
+
+        private void dgSzemelyek_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (dgSzemelyek.SelectedItem is enSzemely sz)
+            {
+                dgTelefonszamok.ItemsSource = sz.enTelefonszamok.ToList();
+            }
         }
     }
 }
